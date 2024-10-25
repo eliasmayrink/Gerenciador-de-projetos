c214edd7dd543a25d5ad72765fc0bed4a4270603
Elias Mayrink <eliasmayrink@gmail.com>
2024-10-24 22:17:08 -0300

    change database to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_10_24_171122) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "activities", force: :cascade do |t|
     t.integer "project_id", null: false
     t.string "name", null: false
